219707133208f265930d7604feb1fac788707eac
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-09-24 01:08:05 +0600

    Chora

--- app/src/main/java/com/example/imtiazaminsajid/banglachora/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/banglachora/MainActivity.java
@@ -1,9 +1,38 @@
 package com.example.imtiazaminsajid.banglachora;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    String [] choraName;
+    String [] choraWritter;
+    String [] choraDetails;
+    int choraPicture[] = {R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,
+            R.drawable.mamar_bari,  
+
+    };
+    int[] choraAudio  = {R.raw.baburam_sapure,
+            R.raw.mamar_bari,
+            R.raw.aam_pata_jora_jora,
+            R.raw.noton_noton_payra,
+            R.raw.oi_dekha_jay_tal_gach,
+            R.raw.itala_bitala,
+            R.raw.honhon_ponpon,
+            R.raw.bhor_holo_dor_kholo,
+            R.raw.megher_kole_rod
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
