b14f7d598e36d09250a5fc28da98bde1f208c050
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-09-25 00:15:51 +0600

    Custom Adapter Done

---
+++ app/src/main/java/com/example/imtiazaminsajid/banglachora/CustomAdapter.java
@@ -0,0 +1,73 @@
+package com.example.imtiazaminsajid.banglachora;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter {
+    String[] choraName;
+    String[] choraWritter;
+    String[] choraDetails;
+    int[] choraPicture;
+    int[] choraAudio;
+    Context context;
+    LayoutInflater inflater;
+
+    public CustomAdapter(String[] choraName, String[] choraWritter, int[] choraPicture, Context context) {
+        this.choraName = choraName;
+        this.choraWritter = choraWritter;
+        this.choraPicture = choraPicture;
+        this.context = context;
+    }
+
+    public CustomAdapter(Context context,
+                         String[] choraName,
+                         String[] choraWritter,
+                         String[] choraDetails,
+                         int[] choraPicture,
+                         int[] choraAudio) {
+
+        this.choraName = choraName;
+        this.choraWritter = choraWritter;
+        this.choraDetails = choraDetails;
+        this.choraPicture = choraPicture;
+        this.choraAudio = choraAudio;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return choraName.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view==null){
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.chora_listview,viewGroup,false);
+        }
+        ImageView imageView = view.findViewById(R.id.choraImageIV);
+        TextView ChoraName = view.findViewById(R.id.ChoraNameTV);
+        TextView ChoraWritter = view.findViewById(R.id.ChoraWritterTV);
+
+        imageView.setImageResource(choraPicture[i]);
+        ChoraName.setText(choraName[i]);
+        ChoraWritter.setText(choraWritter[i]);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/imtiazaminsajid/banglachora/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/banglachora/MainActivity.java
@@ -12,15 +12,15 @@ public class MainActivity extends AppCompatActivity {
     String [] choraName;
     String [] choraWritter;
     String [] choraDetails;
-    int choraPicture[] = {R.drawable.mamar_bari,
+    int choraPicture[] = {R.drawable.baburam_sapure,
             R.drawable.mamar_bari,
-            R.drawable.mamar_bari,
-            R.drawable.mamar_bari,
-            R.drawable.mamar_bari,
-            R.drawable.mamar_bari,
-            R.drawable.mamar_bari,
-            R.drawable.mamar_bari,
-            R.drawable.mamar_bari,  
+            R.drawable.aam_pata_jora_jora,
+            R.drawable.noton_noton_payra,
+            R.drawable.oi_dekha_jay_tal_gach,
+            R.drawable.ital_bital,
+            R.drawable.honhon_ponpon,
+            R.drawable.bhor_holo_dor_kholo,
+            R.drawable.megher_kole_rod,
 
     };
     int[] choraAudio  = {R.raw.baburam_sapure,
@@ -38,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.choraList);
+        choraName = getResources().getStringArray(R.array.ChoraName);
+        choraWritter = getResources().getStringArray(R.array.ChoraWritter);
+        choraDetails = getResources().getStringArray(R.array.Chora);
+
+        CustomAdapter adapter = new CustomAdapter(choraName,choraWritter, choraPicture,this);
+        listView.setAdapter(adapter);
     }
 }
